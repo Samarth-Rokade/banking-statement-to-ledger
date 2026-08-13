8d04ec3392cd903ab430980e88825d10c755be99
Samarth Rokade <rokadesamarth08@gmail.com>
2026-08-13 19:17:01 +0530

    configuration file added

---
+++ frontend/conf/config.ts
@@ -0,0 +1,6 @@
+const conf = {
+    backendUrl: import.meta.env.VITE_BACKEND_URL ?? '',
+    nodeEnv: import.meta.env.VITE_NODE_ENV ?? '',
+}
+
+export default conf 
--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
+import conf from '../../conf/config.ts'
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:8000/api/v1',
+  baseURL: conf.backendUrl || 'http://localhost:8000/api/v1',
 })
-console.log("backedn url", import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:8000/api/v1')
-
 
 apiClient.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token
